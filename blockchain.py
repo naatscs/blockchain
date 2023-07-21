e51384cb119473e483a4f0bc120cd7ecb806d6cb
naatscs <naatscs@gmail.com>
2023-07-21 10:57:47 -0300

    simple structure and mock block

---
+++ blockchain.py
@@ -0,0 +1,45 @@
+import time
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) +1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            }
+        
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+        
+
+    def new_transactios(self, sender, recipient, amount):
+
+        self.current_transactions.append({
+            'sender': sender, 
+            'recipient': recipient, 
+            'amount':amount
+            })
+
+        new_index = self.last_block['index'] + 1
+        return new_index
+        
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
