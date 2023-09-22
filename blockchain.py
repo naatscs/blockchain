c0509f52cab810966a357214c29de7a5960406de
naatscs <naatscs@gmail.com>
2023-09-22 19:24:40 -0300

    adding proof of word

--- blockchain.py
+++ blockchain.py
@@ -35,6 +35,19 @@ class Blockchain(object):
 
         new_index = self.last_block['index'] + 1
         return new_index
+    
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof +=1
+
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "11"
         
     
     @staticmethod
