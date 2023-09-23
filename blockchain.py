c6484722249fd7d6f99c0e16d67588d9cd69935c
naatscs <naatscs@gmail.com>
2023-09-23 12:12:55 -0300

    remove print

--- blockchain.py
+++ blockchain.py
@@ -47,7 +47,7 @@ class Blockchain(object):
     def valid_proof(last_proof, proof):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "00"
+        return guess_hash[:2] == "00"
         
     
     @staticmethod
