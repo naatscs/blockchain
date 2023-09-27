5c07dc4c0b2f654ecf147059a9355cfbbf8dcbae
naatscs <naatscs@gmail.com>
2023-09-27 15:47:07 -0300

    resolve conflicts endpoint

--- app.py
+++ app.py
@@ -25,6 +25,23 @@ def register_nodes():
     }
     return jsonify(response), 201
 
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain is replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
 @app.route('/mine', methods=['GET'])
 def mine():
     last_block = blockchain.last_block
